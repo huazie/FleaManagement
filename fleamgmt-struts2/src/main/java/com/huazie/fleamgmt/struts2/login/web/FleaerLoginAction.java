f17aa64268b580db8c27c4e2556a6a5585ef4db7
huazie <liugh6@asiainfo.com>
2020-01-14 18:21:05 +0800

    huazie_登录Action优化改造_MOD

--- fleamgmt-struts2/src/main/java/com/huazie/fleamgmt/struts2/login/web/FleaerLoginAction.java
+++ fleamgmt-struts2/src/main/java/com/huazie/fleamgmt/struts2/login/web/FleaerLoginAction.java
@@ -1,12 +1,14 @@
 package com.huazie.fleamgmt.struts2.login.web;
 
 import com.huazie.fleamgmt.constant.FleaMgmtConstants;
-import com.huazie.fleamgmt.module.login.pojo.FleaerLogin;
-import com.huazie.fleamgmt.struts2.base.web.LoginLogAction;
+import com.huazie.fleamgmt.struts2.base.web.BaseAction;
 import com.huazie.frame.auth.base.user.entity.FleaAccount;
-import com.huazie.frame.auth.base.user.service.interfaces.IFleaAccountSV;
-import com.huazie.frame.auth.base.user.service.interfaces.IFleaLoginLogSV;
+import com.huazie.frame.auth.common.pojo.login.FleaUserLoginInfo;
+import com.huazie.frame.auth.common.service.interfaces.IFleaUserLoginSV;
+import com.huazie.frame.common.exception.CommonException;
+import com.huazie.frame.common.util.ObjectUtils;
 import com.opensymphony.xwork2.ActionContext;
+import org.apache.struts2.ServletActionContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -21,73 +23,65 @@ import javax.annotation.Resource;
  * @since 1.0.0
  */
 @Controller
-public class FleaerLoginAction extends LoginLogAction {
+public class FleaerLoginAction extends BaseAction {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FleaerLoginAction.class);
     private static final long serialVersionUID = -8632343740482642538L;
 
-    @Resource(name = "accountSVImpl")
-    private IFleaAccountSV accountService;
+    private transient IFleaUserLoginSV fleaUserLoginSV;
 
-    @Resource(name = "fleaLoginLogSV")
-    private IFleaLoginLogSV loginLogService;
+    private FleaUserLoginInfo fleaUserLoginInfo;
 
-    private FleaerLogin fleaerLogin;
-
-    public FleaerLogin getFleaerLogin() {
-        return fleaerLogin;
+    @Resource(name = "fleaUserLoginSV")
+    public void setFleaUserLoginSV(IFleaUserLoginSV fleaUserLoginSV) {
+        this.fleaUserLoginSV = fleaUserLoginSV;
     }
 
-    public void setFleaerLogin(FleaerLogin fleaerLogin) {
-        this.fleaerLogin = fleaerLogin;
+    public FleaUserLoginInfo getFleaUserLoginInfo() {
+        return fleaUserLoginInfo;
+    }
+
+    public void setFleaUserLoginInfo(FleaUserLoginInfo fleaUserLoginInfo) {
+        this.fleaUserLoginInfo = fleaUserLoginInfo;
     }
 
     /**
      * <p> 跳主登录 </p>
      *
      * @return 默认以json格式返回数据
-     * @throws Exception
      * @since 1.0.0
      */
-    public String login() throws Exception {
+    public String login() {
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("FleaerLoginAction##login() start");
         }
 
-        String name = fleaerLogin.getName();
-        String password = fleaerLogin.getPassword();
+        try {
+            ActionContext aContext = ActionContext.getContext();
 
-        if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("FleaerLoginAction##login() NAME={}", name);
-            LOGGER.debug("FleaerLoginAction##login() PASSWORD={}", password);
+            // 跳主的登录
+            FleaAccount fleaAccount = fleaUserLoginSV.login(fleaUserLoginInfo);
+
+            if (ObjectUtils.isNotEmpty(fleaAccount)) {
+                // 将用户的信息写入到session中,并在跳转到主界面获取这个用户的信息
+                // 这是用户的浏览器与web服务器建立的一次会话,会话结束后,该信息也就消失了
+                aContext.getSession().put(FleaMgmtConstants.SessionConstants.SESSION_FLEAER_ACCOUNT, fleaAccount);
+                // 在这边记录登陆日志
+                fleaUserLoginSV.saveLoginLog(fleaAccount.getAccountId(), ServletActionContext.getRequest());
+                this.result.setRetCode(FleaMgmtConstants.ReturnCodeConstants.RETURN_CODE_Y);
+                this.result.setRetMess("亲，恭喜您登录成功呦");
+            }
+
+        } catch (CommonException e) {
+            this.result.setRetCode(FleaMgmtConstants.ReturnCodeConstants.RETURN_CODE_N);
+            this.result.setRetMess(e.getMessage());
         }
 
-        ActionContext aContext = ActionContext.getContext();
-
-//        FleaAccount account = accountService.login(name, password, IAccountValue.ACCOUNT_FLAG_FLEAER); // 跳主的登录
-//
-//        if (account != null) {
-//            // 将用户的信息写入到session中,并在跳转到主界面获取这个用户的信息
-//            // 这是用户的浏览器与web服务器建立的一次会话,会话结束后,该信息也就消失了
-//            aContext.getSession().put(FleaMgmtConstants.SessionConstants.SESSION_FLEAER_ACCOUNT, account);
-//            // 在这边记录登陆日志
-//            this.saveLoginLog(account.getAccountId());
-//            this.result.setRetCode(FleaMgmtConstants.ReturnCodeConstants.RETURN_CODE_Y);
-//            this.result.setRetMess("亲，恭喜您登录成功呦");
-//        } else {
-//            this.result.setRetCode(FleaMgmtConstants.ReturnCodeConstants.RETURN_CODE_N);
-//            this.result.setRetMess("亲,您输入的账号或密码有误呦");
-//        }
         if (LOGGER.isDebugEnabled()) {
             LOGGER.debug("FleaerLoginAction##login() end");
         }
 
         return "json";
-    }
-
-    @Override
-    protected IFleaLoginLogSV getFleaLoginLogSV() {
-        return loginLogService;
     }
 
 }
