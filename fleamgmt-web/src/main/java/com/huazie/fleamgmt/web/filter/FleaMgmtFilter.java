92ba7eac9630bf133b77255523aa30ff734c4c3f
huazie <huazie.lgh@gmail.com>
2020-05-04 01:04:32 +0800

    huazie_fleamgmt接入FleaFrameFilter过滤器_MOD

---
+++ fleamgmt-web/src/main/java/com/huazie/fleamgmt/web/filter/FleaMgmtFilter.java
@@ -0,0 +1,14 @@
+package com.huazie.fleamgmt.web.filter;
+
+import com.huazie.frame.core.filter.FleaFrameFilter;
+
+/**
+ * <p> 跳蚤管家过滤器 </p>
+ *
+ * @author huazie
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class FleaMgmtFilter extends FleaFrameFilter {
+
+}
