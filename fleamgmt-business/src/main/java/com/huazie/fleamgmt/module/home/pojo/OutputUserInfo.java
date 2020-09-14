27975472623ed6bc478d83742c6cf18cd7eee828
huazie <huazie.lgh@gmail.com>
2020-09-14 09:06:31 +0800

    huazie_用户信息POJO类_ADD

--- fleamgmt-business/src/main/java/com/huazie/fleamgmt/module/home/pojo/OutputUserInfo.java
+++ fleamgmt-business/src/main/java/com/huazie/fleamgmt/module/home/pojo/OutputUserInfo.java
@@ -2,6 +2,9 @@ package com.huazie.fleamgmt.module.home.pojo;
 
 import com.huazie.frame.common.pojo.OutputCommonData;
 
+import java.util.List;
+import java.util.Map;
+
 /**
  * <p> 用户信息 </p>
  *
@@ -11,6 +14,25 @@ import com.huazie.frame.common.pojo.OutputCommonData;
  */
 public class OutputUserInfo extends OutputCommonData {
 
+    private static final long serialVersionUID = 6562951839192098570L;
 
+    private Map<String, Object> userInfo; // 用户信息Map集合
 
+    private List<Map<String,Object>> menuList; // 菜单列表
+
+    public Map<String, Object> getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(Map<String, Object> userInfo) {
+        this.userInfo = userInfo;
+    }
+
+    public List<Map<String, Object>> getMenuList() {
+        return menuList;
+    }
+
+    public void setMenuList(List<Map<String, Object>> menuList) {
+        this.menuList = menuList;
+    }
 }
